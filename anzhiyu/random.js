1a908c97abe4cf88a4927b9f5ee1a0c87d7a6e3e
Echo <1552312346@qq.com>
2024-05-07 17:52:52 +0800

    Site updated: 2024-05-07 17:52:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/hello-world/"];function toRandomPost(){
+var posts=["2024/05/01/hello-world/","2024/05/01/CSS+CSS3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
