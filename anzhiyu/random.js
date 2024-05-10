89fd3c7ff20b9aaec814ddb70c6dc6e9553dea3a
Echo <1552312346@qq.com>
2024-05-10 18:11:18 +0800

    Site updated: 2024-05-10 18:11:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/hello-world/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/highlight-dark/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/main/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/highlight/","2024/05/01/CSS+CSS3/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo/"];function toRandomPost(){
+var posts=["2024/05/01/hello-world/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/highlight-dark/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/main/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo_files/highlight/","2024/05/01/CSS+CSS3/","2024/05/07/img/vue/CSS基础+CSS3 - Hexo/","2019/10/10/vue2项目-商城/","2019/10/10/vue2-vue3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
